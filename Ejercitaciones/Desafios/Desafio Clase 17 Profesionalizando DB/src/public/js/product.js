3f08e748fc635c1ec4d884b83eb306c594e79498
Bnx07 <55161821+Bnx07@users.noreply.github.com>
2023-02-15 01:49:38 -0300

    Desafio Clase 17 Update 3 de varias

--- Ejercitaciones/Desafios/Desafio Clase 17 Profesionalizando DB/src/public/js/product.js
+++ Ejercitaciones/Desafios/Desafio Clase 17 Profesionalizando DB/src/public/js/product.js
@@ -18,21 +18,20 @@ const addToCart = async() => {
     let quantity = document.getElementById("quantity").value;
     quantity = parseInt(quantity);
     if (quantity > 0) {
-        console.log(pid);
-        console.log(quantity);
         let result = await fetch(`http://localhost:8080/api/carts/${cart}`, {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify([{
                 _id: pid,
                 quantity: quantity,
-            })
+            }])
         })
         console.log(result);
         Swal.fire({
             background: 'rgb(20,20,20)',
             color: 'rgb(180, 180, 180)',
             title: 'Product added',
+            icon: 'success'
         })
     } else {
         Swal.fire({
