f4b64e4242f0f8e7982f9b894ae699f02bdbc8e0
Bnx07 <benjabastan@gmail.com>
2023-03-13 20:01:48 -0300

    Backup desafio clase 24 debido a implementacion de jwt

--- Ejercitaciones/Desafios/Desafio Clase 24 PreEntrega/src/config/passport.config.js
+++ Ejercitaciones/Desafios/Desafio Clase 24 PreEntrega/src/config/passport.config.js
@@ -3,6 +3,12 @@ import local from "passport-local";
 import userModel from "../dao/models/user.js";
 import {createHash, isValidPassword} from "../utils.js";
 
+import passport from "passport";
+import jwt from 'passport-jwt';
+
+const jwtStrategy = jwt.Strategy;
+const extractJwt = jwt.ExtractJwt;
+
 const localStrategy = local.Strategy;
 const initPassport = () => {
 
@@ -71,6 +77,26 @@ const initPassport = () => {
         
         }
     ))
+
+    passport.use('jwt', new jwtStrategy({
+        jwtFromRequest:extractJwt.fromExtractors([cookieExtractor]), // Recibe la cookie y extrae el token
+        secretOrKey: 'coderSecret' // Esta clave era, por error, distinta a la del jwt generator y eso hacía que me tire bad token signature
+    }, async(jwt_payload, done) => {
+        try {
+            return done(null, jwt_payload)
+        } catch (error) {
+            return done(error);
+        }
+    }
+    ));
+}
+
+export const cookieExtractor = req => {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['coderCookieToken'];
+    }
+    return token
 }
 
 export default initPassport;
